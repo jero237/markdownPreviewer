9cca88806cfcb2fc2da670bd1a743dfc249d9cba
jero237 <jeronimobalestra@gmail.com>
2022-02-06 12:12:48 -0300

    Improved styles

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,7 +1,7 @@
 function Editor (props){
     return (
         <div className="container">
-            <div className="titleContainer"><h2>Editor</h2></div>
+            <div className="titleContainer"><i class="fas fa-code"></i><h2>Editor</h2></div>
             <textarea id="editor" value={props.value} onChange={props.onChange}></textarea>
         </div>
     )
--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -4,7 +4,7 @@ import { marked } from 'marked';
 function Preview(props) {
     return (
         <div className="container">
-            <div className="titleContainer"><h2>Previewer</h2></div>
+            <div className="titleContainer"><i class="fas fa-eye"></i><h2>Previewer</h2></div>
             <div dangerouslySetInnerHTML={{__html: marked(props.value)}} id='preview'/>
         </div>)
 }
